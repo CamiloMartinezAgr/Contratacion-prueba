62221d57fa49ee3fa85275534272bc8dfcd6bee8
CamiloMartinezAgr <117457438+CamiloMartinezAgr@users.noreply.github.com>
2024-11-27 12:09:13 -0500

    Add files via upload

---
+++ js/a-script.js
@@ -0,0 +1,29 @@
+(function ($) {
+  'use strict';
+
+  // Sticky Menu
+  $(window).scroll(function () {
+    if ($('.navigation').offset().top > 100) {
+      $('.navigation').addClass('nav-bg');
+    } else {
+      $('.navigation').removeClass('nav-bg');
+    }
+  });
+
+  // easeInOutExpo Declaration
+  jQuery.extend(jQuery.easing, {
+    easeInOutExpo: function (x, t, b, c, d) {
+      if (t === 0) {
+        return b;
+      }
+      if (t === d) {
+        return b + c;
+      }
+      if ((t /= d / 2) < 1) {
+        return c / 2 * Math.pow(2, 10 * (t - 1));
+      } + b;
+      return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+    }
+  });
+
+})(jQuery);
